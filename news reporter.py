9d17c6ea176b2b66b25d3d459f98e2bc51ca2ecb
Shravan <69118069+Shravan-1908@users.noreply.github.com>
2020-08-31 22:12:35 +0530

    Update news reporter.py

--- news reporter.py
+++ news reporter.py
@@ -6,36 +6,11 @@ def speak(str):
 	  speak.Speak(str)
 
 if __name__ == "__main__":
-	# url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=fbddfcc479954110ba532f7b45dc41d2"
-	# news = requests.get(url).text
-	# print(type(news))
-	# news_dict = json.loads(news)
-	# arts = news_dict['articles']
-
-	# speak("Enter number of articles you want")
-	# n = int(input("Enter number of articles you want: "))
-	# a = 1
-
-	# speak("Today's news highlights are...")
-	# for article in arts:
-	# 	print(article['title'])
-	# 	speak(article['title'])
-	# 	if a==n:
-	# 		break
-	# 	else:
-	# 		speak("Moving on to the next news.")
-	# 		a += 1
-	# 		continue
-
-	# speak("Thanks for listening...")
-
-
-	# my alt 
 	from newsapi import NewsApiClient
 	speak("How many news articles do you want to hear? ")
 	pag = int(input("How many news articles do you want to hear? "))
 
-	newsapi = NewsApiClient(api_key='fbddfcc479954110ba532f7b45dc41d2')
+	newsapi = NewsApiClient(api_key='YOUR NEWS API KEY')
 
 	news = newsapi.get_top_headlines(q="tech", language='en', country='in', category='technology', page_size=pag, page=pag)
 
