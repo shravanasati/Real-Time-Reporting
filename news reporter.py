85e10fad5836163fa3f9a7aa7246bc1ed000316c
Aditi <scholar.aditiasati@gmail.com>
2022-10-03 15:35:19 +0530

    second commit

--- news reporter.py
+++ news reporter.py
@@ -6,13 +6,18 @@ def speak(str):
 	engine.runAndWait()
 	
 if __name__ == "__main__":
-	url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=YOUR API KEY HERE"
+
+	with open("keys.json") as f:
+		text = json.load(f)
+		key = text["News API key"]
+		
+	url = f"https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey={key}"
 	news = requests.get(url).text
 	news_dict = json.loads(news)
 	arts = news_dict['articles']
 	
 	a = 1
-
+	n = 5
 	speak("Today's news highlights are...")
 	for article in arts:
 		print(article['title'])
